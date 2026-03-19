79b7cd7b82b28f21ed43ebcbc2c0de0b4d3f23ae
TechyBear404 <techybear404@gmail.com>
2026-03-19 21:50:40 +0100

    feat(pwa): install and setup Vite-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
     plugins: [
@@ -22,6 +23,33 @@ export default defineConfig({
                     includeAbsolute: false,
                 },
             },
+        }),
+        VitePWA({
+            registerType: 'autoUpdate',
+            workbox: {
+                navigateFallback: null,
+            },
+            manifest: {
+                name: 'Autosaler',
+                short_name: 'Autosaler',
+                start_url: '/',
+                display: 'standalone',
+                background_color: '#ffffff',
+                theme_color: '#1c2431',
+                icons: [
+                    {
+                        src: '/icon-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
+            buildBase: '/build/',
         }),
     ],
     resolve: {
